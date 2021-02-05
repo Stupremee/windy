d4fc08363baed99939046a9c0799f05b4562b57d
Justus K <justus.k@protonmail.com>
2021-02-05 14:13:02 +0100

    Fix flake.nix

--- flake.nix
+++ flake.nix
@@ -3,55 +3,57 @@
 
   inputs = {
     devshell.url = "github:numtide/devshell";
-    naersk.url = "github:nmattia/naersk";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
-    gitignore = {
-      url = "github:hercules-ci/gitignore.nix";
-      flake = false;
-    };
   };
 
-  outputs =
-    { nixpkgs, rust-overlay, naersk, gitignore, devshell, flake-utils, ... }:
+  outputs = { nixpkgs, rust-overlay, devshell, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        version = "0.1.0";
-        pname = "windy";
-
-        inherit (gitignore-lib) gitignoreSource;
         inherit (flake-utils.lib) mkApp;
 
-        gitignore-lib = import gitignore { inherit (pkgs) lib; };
+        pkgsRiscv = import nixpkgs {
+          inherit system;
+          crossSystem = { config = "riscv64-none-elf"; };
+        };
+
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ devshell.overlay rust-overlay.overlay ];
         };
 
+        opensbi = pkgsRiscv.callPackage ./nix/opensbi.nix { };
+        runQemu = pkgs.writers.writeBashBin "run-qemu" ''
+          ${pkgs.qemu}/bin/qemu-system-riscv64 \
+              -machine virt \
+              -cpu rv64 \
+              -smp 4 \
+              -m 128M \
+              -nographic \
+              -serial mon:stdio \
+              -d guest_errors,trace:riscv_trap,trace:sifive_gpio_write,trace:pmpcfg_csr_write,trace:pmpaddr_csr_write,int,trace:exynos_uart_read \
+              -bios ${opensbi}/platform/fw_dynamic.bin \
+              -gdb tcp::1234 \
+              -kernel "$@"
+        '';
         rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
-        naersk-lib = naersk.lib."${system}".override {
-          cargo = rust;
-          rustc = rust;
+      in {
+        # `nix run qemu`
+        apps.qemu = mkApp {
+          name = "run-qemu";
+          drv = runQemu;
         };
-      in rec {
-        # `nix build`
-        packages."${pname}" = naersk-lib.buildPackage {
-          inherit version;
-          root = gitignoreSource ./.;
-        };
-        defaultPackage = packages."${pname}";
-
-        # `nix run`
-        apps."${pname}" = mkApp {
-          name = pname;
-          drv = packages."${pname}";
-        };
-        defaultApp = apps."${pname}";
 
         # `nix develop`
-        devShell = pkgs.devshell.mkShell {
-          name = pname;
-          packages = with pkgs; [ rust ];
+        devShell = pkgs.mkShell {
+          name = "windy-development";
+          nativeBuildInputs = [ rust runQemu ];
+          #packages = [ rust runQemu ];
+          #commands = [{
+          #name = "run-qemu";
+          #help = "Cargo runner for running Windy inside QEMU";
+          #package = runQemu;
+          #}];
         };
       });
 }
